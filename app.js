5ec40a2de2966e76e404e5dae391f5d8afa3bce1
sazzad-anwar <sazzadzihan@gmail.com>
2020-08-31 10:17:45 +0600

    sslify-updated

--- app.js
+++ app.js
@@ -5,11 +5,10 @@ let io = require( 'socket.io' )( server );
 let stream = require( './ws/stream' );
 let morgan = require('morgan');
 let path = require( 'path' );
-// let favicon = require( 'favicon' );
+var enforce = require('express-sslify');
 
 app.use(morgan('common'))
 
-// app.use( favicon( path.join( __dirname, 'favicon.ico' ) ) );
 app.use( '/assets', express.static( path.join( __dirname, 'assets' ) ) );
 
 app.get( '/', ( req, res ) => {
@@ -19,9 +18,14 @@ app.get( '/', ( req, res ) => {
 
 io.of( '/stream' ).on( 'connection', stream );
 
+io.on("connection", ()=>{
+  console.log('connected');
+});
+
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3939;
 }
+app.use(enforce.HTTPS());
 
-server.listen( port,()=>console.log('App is connected on port 3939'));
+server.listen( port,()=>console.log(`App is connected on port ${port}`));
