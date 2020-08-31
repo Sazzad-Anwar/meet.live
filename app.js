42182bca1f37e96709b24a950f2b86610f4d6cfb
mohi445257 <mohi445257@gmail.com>
2020-08-31 06:34:13 +0000

    updated

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ let io = require( 'socket.io' )( server );
 let stream = require( './ws/stream' );
 let morgan = require('morgan');
 let path = require( 'path' );
-var secure = require('ssl-express-www');
+var sslRedirect = require('heroku-ssl-redirect');
+
+app.use(sslRedirect());
 
 app.use(morgan('common'))
 
@@ -28,6 +30,5 @@ if (port == null || port == "") {
   port = 3939;
 }
 
-app.use(secure);
 
 server.listen( port,()=>console.log(`App is connected on port ${port}`));
