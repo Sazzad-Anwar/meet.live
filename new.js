da5b5f38ffb0d0402bc197d60f53756570c3f018
sazzad-anwar <sazzadzihan@gmail.com>
2020-09-21 18:24:37 +0600

    new file added

--- new.js
+++ new.js
@@ -1,11 +1,10 @@
 const bcrypt = require('bcryptjs');
-const { json } = require('express');
 
-// let word = "1234"
+let word = "#Admin123@bluetech.com"
 
-// var salt = bcrypt.genSaltSync(10);
-// var hash = bcrypt.hashSync(word, salt);
+var salt = bcrypt.genSaltSync(10);
+var hash = bcrypt.hashSync(word, salt);
 
-// console.log(`this is hash ${hash}`);
+console.log(`this is hash ${hash}`);
 
-const date = new Date();
+// const date = new Date();
